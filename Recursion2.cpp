9a6387ce30ed46eea4a4bcf193ac627108ba4ab5
DJ5harma <138970319+DJ5harma@users.noreply.github.com>
2023-07-28 12:44:09 +0530

    Update Recursion2.cpp

--- Recursion2.cpp
+++ Recursion2.cpp
@@ -1,1 +1,43 @@
+#include <iostream>
+// #include <string>
+using namespace std;
 
+
+// Remove character
+string rmChar(string str, char Char, string newStr="", int i=0){
+
+    if(i<str.length()){
+        if(str[i] == Char){
+            return rmChar(str, Char, newStr, i+1);
+        }
+        else{
+            return rmChar(str, Char, newStr+str[i], i+1);
+            }
+    }
+    return newStr;
+    
+}
+// Remove substring
+string rmStr(string str, string rm, string newStr="", int i=0){
+    if(i<str.length()){
+        if(str.substr(i, rm.length())==rm){
+            return rmStr(str, rm, newStr, i+rm.length());
+        }
+        else{
+            return rmStr(str, rm, newStr+str[i], i+1);}
+    }
+    return newStr;
+
+}
+
+int main(){cout<<endl;
+
+// Remove character
+    // string str="baccad";
+    // cout<<rmChar(str, 'a');
+
+// Remove substring
+    // string str="an apple a day, apple keeps apple.";
+    // cout<<rmStr(str, " apple");
+
+return 0;}
