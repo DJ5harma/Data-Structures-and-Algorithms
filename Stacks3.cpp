f29275236230ea78263d9766c990c95241145cd6
Dhananjay Bhargav Sharma <138970319+DJ5harma@users.noreply.github.com>
2023-08-08 12:36:05 +0530

    Update Stacks3.cpp

--- Stacks3.cpp
+++ Stacks3.cpp
@@ -1,1 +1,97 @@
+#include<iostream>
+#include<stack>
+#include<math.h>
+using namespace std;
 
+int evaluatePrefixExpression(string &str){
+    stack<int>st;
+
+    for(int i = str.length()-1; i>=0; i--){    
+        if(str[i]>='0' && str[i]<='9'){
+            st.push(str[i]-'0');
+        }
+        else{
+            int op1=st.top();
+            st.pop();
+            int op2=st.top();
+            st.pop();
+
+            switch (str[i])
+            {
+            case '+':
+                st.push(op1+op2);
+                break;
+            case '-':
+                st.push(op1-op2);
+                break;
+            case '*':
+                st.push(op1*op2);
+                break;
+            case '/':
+                st.push(op1/op2);
+                break;
+            case '^':
+                st.push(pow(op1,op2));
+                break;
+            
+            default:
+                break;
+            }
+        }
+    }
+    return st.top();
+}
+
+int evaluatePostfixExpression(string str){
+
+    stack<int>st;
+
+    for(int i=0; i<str.length(); i++){
+        
+        if(str[i]>='0' && str[i]<='9'){
+            st.push(str[i]-'0');
+        }
+        else{
+            int op2=st.top();
+            st.pop();
+            int op1=st.top();
+            st.pop();
+
+            switch (str[i])
+            {
+            case '+':
+                st.push(op1+op2);
+                break;
+            case '-':
+                st.push(op1-op2);
+                break;
+            case '*':
+                st.push(op1*op2);
+                break;
+            case '/':
+                st.push(op1/op2);
+                break;
+            case '^':
+                st.push(pow(op1,op2));
+                break;
+            
+            default:
+                break;
+            }
+        }
+    }
+    return st.top();
+
+}
+
+
+int main(){
+
+string s1 = "-+7*45+20";
+// cout<<evaluatePrefixExpression(s1)<<endl;
+
+string s2 = "46+2/5*7+";
+// cout<<evaluatePostfixExpression(s2);
+
+
+return 0;}
