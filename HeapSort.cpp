041778dffb15a77913631a692ad49e16f1b31bc9
Dhananjay Bhargav Sharma <138970319+DJ5harma@users.noreply.github.com>
2023-10-14 09:15:51 +0530

    Update HeapSort.cpp

--- HeapSort.cpp
+++ HeapSort.cpp
@@ -1,7 +1,7 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-void heapify(int v[], int &i, int &size){
+void heapify(int v[], int i, int &size){
     int smallest = i;
     int l = i*2 + 1;
     int r = i*2 + 2;
@@ -22,14 +22,23 @@ void heapify(int v[], int &i, int &size){
     }
 }
 
-void buildHeap(int v[], int &size){
+void heapSort(int v[], int &size){
     
     for(int i = (size-1)/2; i>=0; --i){
         heapify(v, i, size);
     }
+
+    for(int i=size-1; i>=0; --i){
+        int temp = v[0];
+        v[0]=v[i];
+        v[i]=temp;
+
+        size = i; 
+        heapify(v, 0, size);
+    }
 }
 
-void printArr(int v[], int &size){
+void printArr(int v[], int size){
     for(int o=0; o<size; ++o){
         cout<<v[o]<<' ';
     }
@@ -41,11 +50,7 @@ int main(){
 int v[]={1,3,5,4,6,13,10,9,8,15,17};
 int size = sizeof(v)/sizeof(int);
 
-while (size){
-    buildHeap(v, size);
-    cout<<v[0]<<' ';
-    v[0]=v[size-1];
-    size--;
-}
+heapSort(v, size);
+printArr(v, sizeof(v)/sizeof(int));
 
 return 0;}
